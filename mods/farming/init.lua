4c71ccfa5bef77725c6d41dfd168d72e29f03c6d
Kotolegokot <gkotolegokot@gmail.com>
2012-11-07 22:58:32 +0600

    Drop and groups

--- mods/farming/init.lua
+++ mods/farming/init.lua
@@ -17,8 +17,8 @@ farming.grains={
 	"rape",]]
 }
 farming.grains_desc={
-	"Wheat Hard",
-	"Wheat Soft",
+	"Hard Wheat",
+	"Soft Wheat",
 	"Rye",
 	"Rice",
 	"Oast",
@@ -67,34 +67,52 @@ farming.nuts={
 
 for i, grain in ipairs(farming.grains) do
 	for j = 1,8 do
-	minetest.register_node("farming:"..grain.."_stage_"..j, {
-		description = farming.grains_desc[i],
-		tiles = {"farming_"..grain.."_"..j..".png"},
-		groups = {oddly_breakable_by_hand=2, cracky=3, dig_immediate=1},
-		sounds = default.node_sound_leaves_defaults(),
-		walkable = false,
-		drawtype = "nodebox",
-		paramtype = "light",
-		node_box = {
-			type = "fixed",
-			fixed = {
-				{-0.5,-0.5,-0.3,0.5,0.5,-0.3},
-				{-0.5,-0.5, 0.3,0.5,0.5, 0.3},
-				{-0.3,-0.5,-0.5,-0.3,0.5,0.5},
-				{ 0.3,-0.5,-0.5, 0.3,0.5,0.5},
+		local drop
+		if j == 8 then
+			drop = "farming:"..grain.."_sheaf"
+		else
+			drop = {
+				max_items = 1,
+				items = {
+					{
+						items = {},
+						rarity = 2,
+					},
+					{
+						items = {"farming:"..grain.."_seeds"},
+					}
+				}
+			}
+		end
+		minetest.register_node("farming:"..grain.."_stage_"..j, {
+			description = farming.grains_desc[i],
+			tiles = {"farming_"..grain.."_"..j..".png"},
+			groups = {cracky=3, dig_immediate=2},
+			drop = drop,
+			sounds = default.node_sound_leaves_defaults(),
+			walkable = false,
+			drawtype = "nodebox",
+			paramtype = "light",
+			node_box = {
+				type = "fixed",
+				fixed = {
+					{-0.5,-0.5,-0.3,0.5,0.5,-0.3},
+					{-0.5,-0.5, 0.3,0.5,0.5, 0.3},
+					{-0.3,-0.5,-0.5,-0.3,0.5,0.5},
+					{ 0.3,-0.5,-0.5, 0.3,0.5,0.5},
+				},
 			},
-		},
-		selection_box = {
-			type = "fixed",
-			fixed = {
-				{-0.5,-0.5,-0.5,0.5,-0.5+j/10,0.5},
+			selection_box = {
+				type = "fixed",
+				fixed = {
+					{-0.5,-0.5,-0.5,0.5,-0.5+j/10,0.5},
+				},
 			},
-		},
-		on_construct = function(pos)
-			local meta = minetest.env:get_meta(pos)
-			meta:set_int("progress",j*10)
-		end,
-	})
+			on_construct = function(pos)
+				local meta = minetest.env:get_meta(pos)
+				meta:set_int("progress",j*10)
+			end,
+		})
 	end
 	
 	minetest.register_craftitem("farming:"..grain.."_seeds", {
